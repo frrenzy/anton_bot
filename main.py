e8ff67b31a0fd4ff317301d74806dc41f1c22b01
Ivan Sizov <thecutecinnamonbun@gmail.com>
2022-03-12 18:42:20 +0300

    added pic support

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 import telebot
+from PIL import Image
+from io import BytesIO
+from settings import settings
 
 
-bot = telebot.TeleBot('5173119245:AAH0U_iIkubcTHgW2K-K8iQayEkKVrB5MlA')
-ivan = 445248738
-anton = 365813198
-anton_channel = -1001606988992
-test_channel = -1001614329550
+bot = telebot.TeleBot(settings['token'])
 
 
 def get_latest_post():
-    r = requests.get('https://vk.com/gisinalolstream')
+    r = requests.get(settings['group'])
     soup = BeautifulSoup(r.text, 'html.parser')
-    with open('/opt/anton_bot/post.txt', 'r+') as f:
-        latest_post_id = soup.find_all(class_='pi_author')[0]['data-post-id']
-        if latest_post_id == f.readline():
-            return 0
+    with open('/Users/ivan/PycharmProjects/anton_bot/post.txt', 'r+') as f:
+        latest_post = soup.find(class_='wall_item')
+        latest_post_id = latest_post.find('a', class_='pi_author')['data-post-id']
         f.seek(0)
+        if latest_post_id == f.readline():
+            return None, None
+        f.seek(0)
+        f.truncate()
         f.write(latest_post_id)
 
-    post_strings = [repr(x).strip("'") for x in soup.find_all(class_='pi_text')[0].strings]
+    post_strings = [repr(x).strip("'") for x in latest_post.find(class_='pi_text').strings]
     s = ''
     flag = 0
     for row in post_strings:
@@ -28,11 +30,24 @@ def get_latest_post():
             s += '\n'
             flag = 1
         s += row + '\n'
-    return s
+
+    try:
+        img_source = latest_post.find('div', class_='thumb_map_img_as_div')['data-src_big']
+        r2 = requests.get(img_source)
+        i = Image.open(BytesIO(r2.content))
+        img_path = settings['img_path']
+        i.save(img_path)
+    except TypeError:
+        img_path = None
+    finally:
+        return s, img_path
 
 
 if __name__ == '__main__':
-    text = get_latest_post()
-    if text != 0:
-        bot.send_message(anton_channel, text)
+    text, image = get_latest_post()
+    if text:
+        bot.send_message(settings['channel'], text)
+    if image:
+        with open(image, 'rb') as f:
+            bot.send_photo(settings['channel'], f)
 
---
+++ settings.py
@@ -0,0 +1,28 @@
+DEBUG = True
+
+
+token_prod = '5173119245:AAH0U_iIkubcTHgW2K-K8iQayEkKVrB5MlA'
+token_test = '5222693840:AAE5XhxZ250vaAdwVPexqP8RWWjvCGSjMnY'
+ivan = 445248738
+anton = 365813198
+channel_prod = -1001606988992
+channel_test = -1001614329550
+group_prod = 'https://vk.com/gisinalolstream'
+group_test = 'https://vk.com/club211430881'
+path_test = '/Users/ivan/Library/Application Support/JetBrains/PyCharm2021.3/scratches/test.jpg'
+path_prod = '/opt/anton_bot/post_pic.jpg'
+
+if DEBUG:
+    settings = {
+        'token': token_test,
+        'channel': channel_test,
+        'group': group_test,
+        'img_path': path_test
+    }
+else:
+    settings = {
+        'token': token_prod,
+        'channel': channel_prod,
+        'group': group_prod,
+        'img_path': path_prod
+    }
