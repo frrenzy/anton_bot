421889887621fed4cb74a42d8701d8de07213bea
Ivan Sizov <vsizov63@gmail.com>
2022-03-20 18:54:57 +0300

    preview & caption

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import requests
 import telebot
 from PIL import Image
 from io import BytesIO
-from settings import settings
+from settings import settings, ivan
+import os
 
 
 bot = telebot.TeleBot(settings['token'])
+BASEDIR = os.path.dirname(__file__)
 
 
 def get_latest_post():
     r = requests.get(settings['group'])
     soup = BeautifulSoup(r.text, 'html.parser')
-    with open(settings['post'], 'r+') as f:
+    with open(os.path.join(BASEDIR, 'post.txt'), 'r+') as f:
         latest_post = soup.find(class_='wall_item')
         latest_post_id = latest_post.find('a', class_='pi_author')['data-post-id']
         f.seek(0)
@@ -38,9 +40,11 @@ def get_latest_post():
         img_source = latest_post.find('div', class_='thumb_map_img_as_div')['data-src_big']
         r2 = requests.get(img_source)
         i = Image.open(BytesIO(r2.content))
-        img_path = settings['img_path']
+        img_path = os.path.join(BASEDIR, 'post_pic.jpg')
         i.save(img_path)
     except TypeError:
+        img_path = None
+    except KeyError:
         img_path = None
     finally:
         return s, img_path
@@ -48,9 +52,8 @@ def get_latest_post():
 
 if __name__ == '__main__':
     text, image = get_latest_post()
-    if text:
-        bot.send_message(settings['channel'], text)
     if image:
         with open(image, 'rb') as f:
-            bot.send_photo(settings['channel'], f)
-
+            bot.send_photo(settings['channel'], f, caption=text)
+    elif text:
+        bot.send_message(settings['channel'], text)
